9c5bfef54b12b875f7bbebbf2df677d33d269732
Alexander Essenwanger <cibernop@mable-anex.de>
2024-03-13 09:15:42 +0100

    get read info from config and add checks

--- src/excel_provider/rest/handler.py
+++ src/excel_provider/rest/handler.py
@@ -6,19 +6,47 @@ from excel_provider.io.excel import read_excel
 
 
 class RestHandler:
-    def __init__(self):
+    def __init__(self, handler_config: Dict = None):
         self.data: Dict[str, Dict] = None
         self.sheets: Dict[str, str] = None
+        self.config = handler_config
 
-    def read_data(self, excel_file: Path, sheets: List[str], data_cols: List[str], index_cols: List[int] = None):
-        self.data = read_excel(excel_file, sheets, data_cols, index_cols)
-        self.sheets = {uuid4(): sheet for sheet in sheets}
+    def read_data(self):
+        if self.config is None:
+            raise ValueError("Handler config is not set")
+
+        if not self.config_valid():
+            raise ValueError("Handler config is not valid")
+
+        read_config = {
+            "excel_file": Path(self.config["excel_file"]),
+            "sheets": self.config["sheets"],
+            "data_cols": self.config["data_cols"],
+            "index_cols": self.config.get("index_cols"),
+        }
+
+        self.data = read_excel(**read_config)
+        self.sheets = {uuid4(): sheet for sheet in self.data.keys()}
+
+    def config_valid(self) -> bool:
+        return (
+            self.config is not None
+            and self.config.get("excel_file") is not None
+            and isinstance(self.config.get("sheets"), list)
+            and isinstance(self.config.get("data_cols"), list)
+        )
 
     def get_data(self, sheet_id: str) -> Dict:
+        if self.sheets is None or self.data is None:
+            raise ValueError("Data has not been read")
+
         if sheet := self.sheets.get(sheet_id):
             return self.data[sheet]
         else:
             raise ValueError(f"Sheet with id {sheet_id} does not exist")
 
     def get_sheet_names(self) -> Dict[str, str]:
+        if self.sheets is None:
+            raise ValueError("Data has not been read")
+
         return self.sheets
--- tests/rest/test_handler.py
+++ tests/rest/test_handler.py
@@ -7,16 +7,63 @@ def test_get_sheet_names():
 
     assert handler.get_sheet_names() == {"1": "Sheet1", "2": "Sheet2"}
 
+
+def test_get_sheet_names_no_data():
+    handler = RestHandler()
+
+    try:
+        handler.get_sheet_names()
+    except ValueError as e:
+        assert str(e) == "Data has not been read"
+    else:
+        assert False
+
+
 def test_get_data():
     handler = RestHandler()
-    handler.data = {"Sheet1": {"Value": {1: 1, 2: 2}}, "Sheet2": {"Name": {1: "Jane", 2: "John"}}}
+    handler.data = {
+        "Sheet1": {"Value": {1: 1, 2: 2}},
+        "Sheet2": {"Name": {1: "Jane", 2: "John"}},
+    }
     handler.sheets = {"1": "Sheet1", "2": "Sheet2"}
 
     assert handler.get_data("1") == {"Value": {1: 1, 2: 2}}
     assert handler.get_data("2") == {"Name": {1: "Jane", 2: "John"}}
 
+
+def test_get_data_no_data():
+    handler = RestHandler()
+    handler.sheets = {"1": "Sheet1", "2": "Sheet2"}
+
+    try:
+        handler.get_data("1")
+    except ValueError as e:
+        assert str(e) == "Data has not been read"
+    else:
+        assert False
+
+
+def test_get_data_no_sheets():
+    handler = RestHandler()
+    handler.data = {
+        "Sheet1": {"Value": {1: 1, 2: 2}},
+        "Sheet2": {"Name": {1: "Jane", 2: "John"}},
+    }
+
+    try:
+        handler.get_data("1")
+    except ValueError as e:
+        assert str(e) == "Data has not been read"
+    else:
+        assert False
+
+
 def test_get_data_sheet_not_found():
     handler = RestHandler()
+    handler.data = {
+        "Sheet1": {"Value": {1: 1, 2: 2}},
+        "Sheet2": {"Name": {1: "Jane", 2: "John"}},
+    }
     handler.sheets = {"1": "Sheet1", "2": "Sheet2"}
 
     try:
@@ -25,3 +72,69 @@ def test_get_data_sheet_not_found():
         assert str(e) == "Sheet with id 3 does not exist"
     else:
         assert False
+
+
+def test_config_valid():
+    handler = RestHandler(
+        handler_config={
+            "excel_file": "test.xlsx",
+            "sheets": ["Sheet1", "Sheet2"],
+            "data_cols": ["Value", "Name"],
+        }
+    )
+
+    assert handler.config_valid() == True
+
+
+def test_config_valid_no_config():
+    handler = RestHandler()
+
+    assert handler.config_valid() == False
+
+
+def test_config_valid_no_excel_file():
+    handler = RestHandler(
+        handler_config={"sheets": ["Sheet1", "Sheet2"], "data_cols": ["Value", "Name"]}
+    )
+
+    assert handler.config_valid() == False
+
+
+def test_config_valid_no_sheets():
+    handler = RestHandler(
+        handler_config={"excel_file": "test.xlsx", "data_cols": ["Value", "Name"]}
+    )
+
+    assert handler.config_valid() == False
+
+
+def test_config_valid_sheets_no_list():
+    handler = RestHandler(
+        handler_config={
+            "excel_file": "test.xlsx",
+            "data_cols": ["Value", "Name"],
+            "sheets": "Sheet1",
+        }
+    )
+
+    assert handler.config_valid() == False
+
+
+def test_config_valid_no_data_cols():
+    handler = RestHandler(
+        handler_config={"excel_file": "test.xlsx", "sheets": ["Sheet1", "Sheet2"]}
+    )
+
+    assert handler.config_valid() == False
+
+
+def test_config_valid_data_cols_no_list():
+    handler = RestHandler(
+        handler_config={
+            "excel_file": "test.xlsx",
+            "sheets": ["Sheet1", "Sheet2"],
+            "data_cols": "Value",
+        }
+    )
+
+    assert handler.config_valid() == False
